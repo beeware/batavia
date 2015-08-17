271194cf39a2561b86e97783512f6d17dc959de6
Sławomir Zborowski <slawomir.zborowski@nsn.com>
2015-08-18 01:05:38 +0200

    Added possibility to run custom code through testbed. However, plz keep in mind that this is pretty insecure

--- testserver/urls.py
+++ testserver/urls.py
@@ -1,26 +1,37 @@
 import os
 import base64
 import py_compile
+import tempfile
 
 from django.conf.urls import url
 from django.shortcuts import render
 
 
 def bytecode(sourcefile):
-    py_compile.compile(sourcefile)
-    with open(os.path.join(
-                os.path.dirname(sourcefile),
-                '__pycache__/%s.cpython-34.pyc' % os.path.splitext(os.path.basename(sourcefile))[0]
-            ), 'rb') as compiled:
-        payload = base64.encodebytes(compiled.read())
-    return payload
+    with tempfile.NamedTemporaryFile() as temp:
+        py_compile.compile(sourcefile, cfile=temp.name)
+        with open(os.path.join(
+                    os.path.dirname(sourcefile),
+                    temp.name
+                ), 'rb') as compiled:
+            payload = base64.encodebytes(compiled.read())
+        return payload
 
 
 def home(request):
-    return render(request, 'testbed.html', {
+    ctx = {
         'samplecode': bytecode('sample.py'),
         'othercode': bytecode('other.py')
-    })
+    }
+    if request.method.lower() == 'post' and request.POST['code']:
+        with tempfile.NamedTemporaryFile() as temp:
+            temp.write(bytes(request.POST['code'], 'utf-8'))
+            temp.flush()
+            ctx['customcode'] = {
+                'code': request.POST['code'],
+                'compiled': bytecode(temp.name)
+            }
+    return render(request, 'testbed.html', ctx)
 
 
 urlpatterns = [
