8b33fb0e925b36cb97aa51164cf082948a81a858
FeralBytes <feralbytes@gmail.com>
2016-05-05 21:57:23 -0400

    Try to ensure manage.py is launched using py3.

--- testserver/manage.py
+++ testserver/manage.py
@@ -3,6 +3,14 @@ import os
 import sys
 
 if __name__ == "__main__":
+    
+    if 'python3' not in sys.executable:  # might be better to check version also.
+    # if sys.hexversion < 0x03000000:
+        python3_path = distutils.spawn.find_executable('python3')
+        if python3_path is not None:
+            os.execvp(sys.executable + '3', [sys.executable + '3'] + sys.argv)
+            #TODO what to do if it is None?
+    
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from django.core.management import execute_from_command_line
