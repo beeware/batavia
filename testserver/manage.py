6ce222d77f025e9501790f0340bbb272260e5fb2
FeralBytes <feralbytes@gmail.com>
2016-05-06 21:22:54 -0400

    Updated to address the comments.

--- testserver/manage.py
+++ testserver/manage.py
@@ -4,13 +4,14 @@ import sys
 
 if __name__ == "__main__":
     
-    if 'python3' not in sys.executable:  # might be better to check version also.
-    # if sys.hexversion < 0x03000000:
+    if sys.version_info[0] != 3:
         import distutils.spawn
         python3_path = distutils.spawn.find_executable('python3')
         if python3_path is not None:
             os.execvp(sys.executable + '3', [sys.executable + '3'] + sys.argv)
-            #TODO what to do if it is None?
+        else:
+            sys.stderr.write('Batavia requires Python 3' + os.linesep)
+            sys.exit(1)
     
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
