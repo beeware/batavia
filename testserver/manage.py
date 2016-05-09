abb86de8d8a03d5e08239f03e9cdd82290b89e03
FeralBytes <feralbytes@gmail.com>
2016-05-09 10:24:31 -0400

    Simply print error and exit; require py3.

--- testserver/manage.py
+++ testserver/manage.py
@@ -5,13 +5,8 @@ import sys
 if __name__ == "__main__":
     
     if sys.version_info[0] != 3:
-        import distutils.spawn
-        python3_path = distutils.spawn.find_executable('python3')
-        if python3_path is not None:
-            os.execvp(sys.executable + '3', [sys.executable + '3'] + sys.argv)
-        else:
-            sys.stderr.write('Batavia requires Python 3' + os.linesep)
-            sys.exit(1)
+        sys.stderr.write('Batavia requires Python 3' + os.linesep)
+        sys.exit(1)
     
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
