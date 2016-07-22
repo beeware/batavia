bff40d987c3912342ef73aa4a2abdd2a0a80747a
Kevin Carr <cochewen@gmail.com>
2016-07-22 12:33:51 -0400

    resolved + <class 'TypeError'> : unsupported operand type(s) for |=: 'str' and 'type' error by adding different file exceptions for Str.js file

--- batavia/types/Str.js
+++ batavia/types/Str.js
@@ -431,7 +431,15 @@ String.prototype.__ior__ = function(other) {
             batavia.types.List,
             batavia.types.NoneType,
             batavia.types.Str,
-            batavia.types.Tuple
+            batavia.types.Tuple,
+            batavia.types.Type,
+            batavia.types.Range,
+            batavia.types.NotImplementedType,
+            batavia.types.Set,
+            batavia.types.Slice,
+
+
+
         ])) {
         throw new batavia.builtins.TypeError("unsupported operand type(s) for |=: 'str' and '" + batavia.type_name(other) + "'");
     } else {
