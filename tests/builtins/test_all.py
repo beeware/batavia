16eabbbda1adbcb12a7aabcb64e0fc02a1186a36
Kirt Gittens <manbitesdog326@gmail.com>
2016-07-28 22:50:14 -0400

    added tests for other cases of the 'all' builtin

--- tests/builtins/test_all.py
+++ tests/builtins/test_all.py
@@ -6,6 +6,15 @@ class AllTests(TranspileTestCase):
     def test_all(self):
         self.assertCodeExecution("print(all([None, True, False]))")
 
+    def test_all_true(self):
+        self.assertCodeExecution("print(all([1,True,3]))")
+
+    def test_all_false(self):
+        self.assertCodeExecution("print(all([0, '', 0.0]))")
+
+    def test_all_empty_list(self):
+        self.assertCodeExecution("print(all([]))")
+
     def test_all_typeerror(self):
         self.assertCodeExecution("print(all(None))")
 
