5bdacc8215d4e657d87d892c5560011886761f83
Christopher Swenson <chris@caswenson.com>
2016-08-15 15:56:27 +1000

    Builtin pow function support for large arguments
    This helps with https://github.com/pybee/batavia/issues/47, as I am a
    first-timer!
    
    There are still some issues with very large arguments, but
    I can file a separate bug for that, assuming it is not already known.

--- batavia/core/builtins.js
+++ batavia/core/builtins.js
@@ -845,7 +845,34 @@ batavia.builtins.pow = function(args) {
         x = args[0];
         y = args[1];
         z = args[2];
-        return Math.pow(x, y) % z;
+
+        if (!batavia.isinstance(x, batavia.types.Int) ||
+            !batavia.isinstance(y, batavia.types.Int) ||
+            !batavia.isinstance(y, batavia.types.Int)) {
+            throw new batavia.builtins.TypeError("pow() requires all arguments be integers when 3 arguments are present");
+        }
+        if (y < 0) {
+          throw new batavia.builtins.TypeError("Builtin Batavia does not support negative exponents")
+        }
+        if (y == 0) {
+          return 1;
+        }
+        if (z == 1) {
+          return 0;
+        }
+
+        // right-to-left exponentiation to reduce memory and time
+        // See https://en.wikipedia.org/wiki/Modular_exponentiation#Right-to-left_binary_method
+        var result = 1;
+        var base = x % z;
+        while (y > 0) {
+          if ((y & 1) == 1) {
+            result = (result * base) % z;
+          }
+          y >>= 1;
+          base = (base * base) % z;
+        }
+        return result;
     } else {
         throw new batavia.builtins.TypeError("pow expected at least 2 arguments, got " + args.length);
     }
