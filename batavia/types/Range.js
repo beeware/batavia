3089f492b7a8658c92f9fdd3b9dcddeba2f2f027
Christopher Swenson <chris@caswenson.com>
2016-08-23 21:30:06 -0700

    range arguments should be ints

--- batavia/types/Range.js
+++ batavia/types/Range.js
@@ -3,10 +3,11 @@
  *************************************************************************/
 
 batavia.types.Range = function() {
+    // BUG: Range supports longs.
     function Range(start, stop, step) {
-        this.start = start;
-        this.stop = stop;
-        this.step = step || 1;
+        this.start = start.int32();
+        this.stop = stop.int32();
+        this.step = new batavia.types.Int(step || 1).int32();
 
         if (this.stop === undefined) {
             this.start = 0;
