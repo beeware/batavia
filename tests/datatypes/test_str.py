043733b9445f373bf6c054c3f649ea1863136c8c
Jacob Stoebel <jstoebel@gmail.com>
2016-08-31 17:34:28 -0400

    Iss46 (#248)
    * added __ge__ for Str
    
    frozenset is not yet implemented so this type was not yet included
    
    * added __lt__, __le__, __gt__

--- tests/datatypes/test_str.py
+++ tests/datatypes/test_str.py
@@ -119,45 +119,15 @@ class BinaryStrOperationTests(BinaryOperationTestCase, TranspileTestCase):
         'test_floor_divide_complex',
         'test_floor_divide_frozenset',
 
-        'test_ge_bytearray',
-        'test_ge_bytes',
-        'test_ge_class',
-        'test_ge_complex',
         'test_ge_frozenset',
-        'test_ge_NotImplemented',
-        'test_ge_range',
-        'test_ge_slice',
 
-        'test_gt_bytearray',
-        'test_gt_bytes',
-        'test_gt_class',
-        'test_gt_complex',
         'test_gt_frozenset',
-        'test_gt_NotImplemented',
-        'test_gt_range',
-        'test_gt_slice',
 
-
-        'test_le_bytes',
-        'test_le_class',
-        'test_le_complex',
         'test_le_frozenset',
-        'test_le_NotImplemented',
-        'test_le_range',
-        'test_le_slice',
-
 
         'test_lshift_frozenset',
 
-        'test_lt_bytearray',
-        'test_lt_bytes',
-        'test_lt_class',
-        'test_lt_complex',
         'test_lt_frozenset',
-        'test_lt_NotImplemented',
-        'test_lt_range',
-        'test_lt_set',
-        'test_lt_slice',
 
         'test_modulo_bool',
         'test_modulo_bytearray',
