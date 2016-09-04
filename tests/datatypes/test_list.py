51ace91a58aea6c4a81d4d569309db05f09b176e
Jacob Stoebel <jstoebel@gmail.com>
2016-09-04 16:53:58 -0400

    Iss46 (#255)
    * implemented operators for list
    
    __eq__
    __pos__
    __neg__
    __not__
    __invert__
    
    * removed commented out not_implemented tests

--- tests/datatypes/test_list.py
+++ tests/datatypes/test_list.py
@@ -143,10 +143,6 @@ class UnaryListOperationTests(UnaryOperationTestCase, TranspileTestCase):
     data_type = 'list'
 
     not_implemented = [
-        'test_unary_invert',
-        'test_unary_negative',
-        'test_unary_not',
-        'test_unary_positive',
     ]
 
 
@@ -159,7 +155,6 @@ class BinaryListOperationTests(BinaryOperationTestCase, TranspileTestCase):
         'test_and_frozenset',
 
         'test_eq_frozenset',
-        'test_eq_list',
 
         'test_floor_divide_complex',
         'test_floor_divide_frozenset',
