d6bc6fb20b17f2f62e706eb77fffb5a6af7a4c06
Christopher Swenson <chris@caswenson.com>
2016-09-05 18:29:16 -0700

    Remove comment about time.clock being incorrect

--- batavia/modules/time.js
+++ batavia/modules/time.js
@@ -2,7 +2,6 @@
 batavia.modules.time = {
     _startTime: new Date().getTime(),
     clock: function() {
-        // TODO: this does something a little different in CPython.
         return new batavia.types.Float(new Date().getTime() - batavia.modules.time._startTime);
     },
 
