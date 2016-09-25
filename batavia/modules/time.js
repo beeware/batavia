1fb05cb585227d72f95a099e585f864b253a47ac
Jacob Stoebel <stoebelj@berea.edu>
2016-09-25 12:14:00 -0400

    start of struct_time and tests

--- batavia/modules/time.js
+++ batavia/modules/time.js
@@ -23,3 +23,54 @@ batavia.modules.time = {
         }
     }
 };
+
+batavia.modules.time.struct_time = function (sequence) {
+    /*
+        copied from https://docs.python.org/2/library/time.html#time.struct_time
+        WHAT ATTRIBUTION DOES THIS NEED?
+
+        0 	tm_year 	(for example, 1993)
+        1 	tm_mon 	    range [1, 12]
+        2 	tm_mday 	range [1, 31]
+        3 	tm_hour 	range [0, 23]
+        4 	tm_min 	    range [0, 59]
+        5 	tm_sec 	    range [0, 61]; see (2) in strftime() description
+        6 	tm_wday 	range [0, 6], Monday is 0
+        7 	tm_yday 	range [1, 366]
+        8 	tm_isdst 	0, 1 or -1; see below
+    */
+
+    if (batavia.isinstance(sequence, [batavia.types.Bytearray, batavia.types.Bytes, batavia.types.Dict,
+        batavia.types.FrozenSet, batavia.types.List, batavia.types.Range, batavia.types.Set, batavia.types.Str,
+        batavia.types.Tuple]
+        )){
+
+        if (sequence.length !== 9){
+            throw new batavia.builtins.TypeError("time.struct_time() takes an at least 9-sequence ("+sequence.length+"-sequence given)")
+        }
+
+        //TODO: complete this block
+        this.n_fields = 9;
+        this.n_unnamed_fields = 9;
+        this.n_sequence_fields = 9;
+
+
+        this.push.apply(this, sequence);
+
+        var attrs = [ "tm_year", "tm_mon", "tm_mday", "tm_hour", "tm_min", "tm_sec", "tm_wday", "tm_yday", "tm_isdst"]
+
+        for (var i=0; i<attrs.length; i++){
+            this[attrs[i]] = sequence[i];
+        }
+
+    } else {
+        //some other, unacceptable type
+        throw new batavia.builtins.TypeError("constructor requires a sequence");
+    }
+}
+
+batavia.modules.time.struct_time.prototype = new batavia.types.Tuple();
+
+batavia.modules.time.struct_time.prototype.__str__ = function(){
+    return "time.struct_time(tm_year="+this.tm_year+", tm_mon="+this.tm_mon+", tm_mday="+this.tm_mday+", tm_hour="+this.tm_hour+", tm_min="+this.tm_min+", tm_sec="+this.tm_sec+", tm_wday="+this.tm_wday+", tm_yday="+this.tm_yday+", tm_isdst="+this.tm_isdst+")"
+}
--- tests/modules/test_time.py
+++ tests/modules/test_time.py
@@ -1,20 +1,17 @@
 from ..utils import TranspileTestCase
 
 import unittest
-
+import time
 
 class TimeTests(TranspileTestCase):
     @unittest.expectedFailure
     def test_time(self):
         self.assertCodeExecution("""
             import time
-
             # Demonstrate that we're getting the same type...
             print(type(time.time()))
-
             # ...and that type is infact a float.
             print(isinstance(time.time(), float))
-
             print('Done.')
             """)
 
@@ -34,3 +31,30 @@ class TimeTests(TranspileTestCase):
                 print(err)
             print('Done.')
             """)
+
+    def test_struct_time_valid(self):
+        # valid construction
+
+        seed = list(range(1, 10))
+        sequences = (
+            bytearray(seed),
+            bytes(seed),
+            dict(zip(seed, seed)),
+            frozenset(seed),
+            seed,
+            range(1, 10),
+            set(seed),
+            ''.join([str(i) for i in seed]),
+            tuple(seed)
+        )
+
+        for seq in sequences:
+            st = time.struct_time(seq)
+            self.assertCodeExecution("""
+            print('>>> import time')
+            import time
+            print(">>> st = time.struct_time({seq})")
+            st = time.struct_time({seq})
+            print('>>> st')
+            print(st)
+            """.format(seq=seq))
