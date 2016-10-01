9e6ee4283bddddc0524f35c02af7c418030001f6
Jacob Stoebel <stoebelj@berea.edu>
2016-10-01 15:51:49 -0400

    syntax fix in struct_time

--- batavia/modules/time.js
+++ batavia/modules/time.js
@@ -26,18 +26,20 @@ batavia.modules.time = {
 
 batavia.modules.time.struct_time = function (sequence) {
     /*
-        copied from https://docs.python.org/2/library/time.html#time.struct_time
-        WHAT ATTRIBUTION DOES THIS NEED?
+        copied from https://docs.python.org/3/library/time.html#time.struct_time
 
-        0 	tm_year 	(for example, 1993)
-        1 	tm_mon 	    range [1, 12]
-        2 	tm_mday 	range [1, 31]
-        3 	tm_hour 	range [0, 23]
-        4 	tm_min 	    range [0, 59]
-        5 	tm_sec 	    range [0, 61]; see (2) in strftime() description
-        6 	tm_wday 	range [0, 6], Monday is 0
-        7 	tm_yday 	range [1, 366]
-        8 	tm_isdst 	0, 1 or -1; see below
+        Index 	Attribute 	Values
+        0 	    tm_year 	(for example, 1993)
+        1 	    tm_mon 	    range [1, 12]
+        2 	    tm_mday 	range [1, 31]
+        3 	    tm_hour 	range [0, 23]
+        4 	    tm_min 	    range [0, 59]
+        5 	    tm_sec 	    range [0, 61]; see (2) in strftime() description
+        6 	    tm_wday 	range [0, 6], Monday is 0
+        7 	    tm_yday 	range [1, 366]
+        8 	    tm_isdst 	0, 1 or -1; see below
+        N/A 	tm_zone 	abbreviation of timezone name
+        N/A 	tm_gmtoff 	offset east of UTC in seconds
     */
 
 
@@ -64,7 +66,7 @@ batavia.modules.time.struct_time = function (sequence) {
 
             var items = new batavia.types.Tuple(sequence);
 
-        } else if (batavia.isinstance(sequence, batavia.types.Dict){
+        } else if (batavia.isinstance(sequence, batavia.types.Dict)){
 
             var items = sequence.keys();
 
