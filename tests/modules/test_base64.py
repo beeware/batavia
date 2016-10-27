7950bee95b56093c28b921c4cfe2531660ac9f78
martini97 <alessandrofmartini@gmail.com>
2016-10-26 22:27:49 -0200

    Added the new tests
    Signed-off-by: martini97 <alessandrofmartini@gmail.com>

--- tests/modules/test_base64.py
+++ tests/modules/test_base64.py
@@ -7,6 +7,7 @@ class Base64Tests(ModuleFunctionTestCase, TranspileTestCase):
             import base64
             print(base64.b64encode(b'foo'))
             print(type(base64.b64encode(b'foo')))
+            print(base64.b64encode(b'\xfb\xfc\xe6', b'-_'))
             """)
 
     def test_b64decode(self):
@@ -14,6 +15,7 @@ class Base64Tests(ModuleFunctionTestCase, TranspileTestCase):
             import base64
             print(str(base64.b64decode(b'Zm9v')))
             print(type(base64.b64decode(b'Zm9v')))
+            print(base64.b64decode(b'Zm9vKy8='))
             """)
 
     def test_decodestring(self):
@@ -28,6 +30,7 @@ class Base64Tests(ModuleFunctionTestCase, TranspileTestCase):
             import base64
             print(str(base64.urlsafe_b64decode(b'aHR0cDovL2dvb2dsZS5jb20=')))
             print(type(base64.b64decode(b'aHR0cDovL2dvb2dsZS5jb20=')))
+            print(base64.urlsafe_b64decode(b'Zm9vYmFyYmF6LisvLmNvbQ=='))
             """)
 
     def test_urlsafe_b64encode(self):
@@ -35,4 +38,5 @@ class Base64Tests(ModuleFunctionTestCase, TranspileTestCase):
             import base64
             print(base64.urlsafe_b64encode(b'http://www.google.com/'))
             print(type(base64.urlsafe_b64encode(b'http://www.google.com/')))
+            print(base64.urlsafe_b64encode(b'~\x8a\x1bj\xb6\xda\xcf\xef\xdc'))
             """)
