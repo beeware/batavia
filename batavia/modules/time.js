d125e40a36da6407089090c7949d1e7c1f9e8234
Russell Keith-Magee <russell@keith-magee.com>
2016-12-03 23:21:39 +0800

    Make a special case of UTC in time.mktime.

--- batavia/modules/time.js
+++ batavia/modules/time.js
@@ -155,12 +155,10 @@ time.mktime = function(sequence) {
     var seconds = date.getTime() / 1000;
 
     // If the struct_time specifies DST (sequence[8] === 1), but
-    // the local timezone doesn't have daylight savings, the answer will
+    // the local timezone is anything but UTC, the answer will
     // be out by 3600 seconds. Adjust accordingly.
     var tz = moment.tz.guess();
-    var isDST = moment(date.getTime()).tz(tz).isDST();
-    console.log("DEBUG: " + tz + " " + isDST + " " + seconds);
-    if (!isDST && sequence[8] == 1) {
+    if (tz !== 'UTC' && sequence[8] == 1) {
         seconds = seconds - 3600;
     }
 
