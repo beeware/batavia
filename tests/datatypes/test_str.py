5d23c3d09493aac8cc1f90c8ea628e6be1e57131
Jacob Stoebel <stoebelj@berea.edu>
2016-12-04 10:00:00 -0500

    expect additional test to pass

--- tests/datatypes/test_str.py
+++ tests/datatypes/test_str.py
@@ -205,22 +205,12 @@ class BinaryStrOperationTests(BinaryOperationTestCase, TranspileTestCase):
     data_type = 'str'
 
     not_implemented = [
-        'test_modulo_bool',
         'test_modulo_bytearray',
-        'test_modulo_bytes',
         'test_modulo_class',
-        'test_modulo_complex',
-        'test_modulo_dict',
-        'test_modulo_float',
-        'test_modulo_frozenset',
-        'test_modulo_int',
-        'test_modulo_list',
         'test_modulo_None',
         'test_modulo_NotImplemented',
         'test_modulo_range',
-        'test_modulo_set',
         'test_modulo_slice',
-        'test_modulo_tuple',
     ]
 
 
