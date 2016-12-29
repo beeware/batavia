a014b7f83d9d857dae1356606b0d79e02a1fc514
Russell Keith-Magee <russell@keith-magee.com>
2016-12-29 17:30:13 +0800

    Corrected scoping on some variables used in divmod.

--- batavia/builtins/divmod.js
+++ batavia/builtins/divmod.js
@@ -13,8 +13,8 @@ function divmod(args, kwargs) {
         throw new exceptions.TypeError('divmod() expected exactly 2 argument (' + args.length + ' given)');
     }
 
-    div = Math.floor(args[0]/args[1]);
-    rem = args[0] % args[1];
+    var div = Math.floor(args[0] / args[1]);
+    var rem = args[0] % args[1];
     return new types.Tuple([new types.Int(div), new types.Int(rem)]);
 }
 divmod.__doc__ = 'Return the tuple ((x-x%y)/y, x%y).  Invariant: div*y + mod == x.';
