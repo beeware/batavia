fcd9237241ad531cba12dd3b00b7398457c86cd0
Kenneth Love <kennethlove@gmail.com>
2017-01-05 19:41:06 -0800

    Methods require dots, not syntax errors

--- tests/datatypes/test_str.py
+++ tests/datatypes/test_str.py
@@ -132,12 +132,12 @@ class StrTests(TranspileTestCase):
             else:
                 print('BeeWare does not start with BeeWare-shaves-yaks')
             # Empty string
-            if 'BeeWare' startswith(''):
+            if 'BeeWare'.startswith(''):
                 print('BeeWare starts with an empty string')
             else:
                 print('BeeWare does not start with an empty string')
             # Invalid type
-            if 'BeeWare' startswith(5):
+            if 'BeeWare'.startswith(5):
                 print('BeeWare starts with the number 5')
             else:
                 print('BeeWare does not start with the number 5')
