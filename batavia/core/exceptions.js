bb5edef855b8057dba4a935d2a19cdf51f46b1e5
Jacob Stoebel <jstoebel@gmail.com>
2017-01-30 09:11:13 -0500

    properly handle strings in KeyError
    need to return __str__ not __repr__

--- batavia/core/exceptions.js
+++ batavia/core/exceptions.js
@@ -1,5 +1,6 @@
 var PyObject = require('./types/Object');
 var Type = require('./types/Type').Type;
+var types = require('../types');
 
 var exceptions = {};
 
@@ -171,6 +172,8 @@ exceptions.KeyError = function (key) {
     var msg;
     if (key === null) {
         msg = "None";
+    } else if (types.isinstance(key, types.Str)) {
+        msg = key.__str__();
     } else if (key['__repr__'] && !key.hasOwnProperty('__repr__')) {
         msg = key.__repr__();
     } else {
