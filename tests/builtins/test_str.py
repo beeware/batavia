ff53acf1e54c1ea39904195342c657aad0ac9079
Becky Smith <rebkwok@gmail.com>
2017-04-09 10:21:59 +0100

    Fix error in builtins.test_str.StrTests Test str() instead of repr()
    Signed-off-by: Becky Smith <rebkwok@gmail.com>

--- tests/builtins/test_str.py
+++ tests/builtins/test_str.py
@@ -4,7 +4,7 @@ from .. utils import TranspileTestCase, BuiltinFunctionTestCase
 class StrTests(TranspileTestCase):
     def test_escape(self):
         self.assertCodeExecution("""
-            print(repr("".join(chr(x) for x in range(128))))
+            print(str("".join(chr(x) for x in range(128))))
             """)
 
 
