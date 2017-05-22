76677d3a547e634e133a63ccb471d0937f938390
Liam Schumm <liamschumm@icloud.com>
2017-05-22 14:47:14 -0700

    Make batavia/modules/webbrowser.js eslint-compliant

--- batavia/modules/webbrowser.js
+++ batavia/modules/webbrowser.js
@@ -4,26 +4,26 @@ var webbrowser = {
     __doc__: '',
     __file__: 'batavia/modules/webbrowser.js',
     __name__: 'webbrowser',
-    __package__: '',
+    __package__: ''
 }
 
 webbrowser.open = function(url) {
-    window.location = url;
+    window.location = url
 }
 
 webbrowser.open_new = function(url) {
-    window.open(url);
+    window.open(url)
 }
 
 // no differentiation inside browser
-webbrowser.open_new_tab = webbrowser.open_new;
+webbrowser.open_new_tab = webbrowser.open_new
 
 webbrowser.get = function(name) {
-    throw new exceptions.NotImplementedError.$pyclass("Multiple web browsers not supported in DOM-embedded webbrowser module; only the host browser.");
+    throw new exceptions.NotImplementedError.$pyclass('Multiple web browsers not supported in DOM-embedded webbrowser module; only the host browser.')
 }
 
 webbrowser.register = function(name, constructor) {
-    throw new exceptions.NotImplementedError.$pyclass("Multiple web browsers not supported in DOM-embedded webbrowser module; only the host browser.");
+    throw new exceptions.NotImplementedError.$pyclass('Multiple web browsers not supported in DOM-embedded webbrowser module; only the host browser.')
 }
 
 module.exports = webbrowser
