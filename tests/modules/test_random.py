469946db546b0d446d214c1e234596611e1456ef
HelloMelanieC <hellomelaniec@gmail.com>
2017-05-22 14:07:58 -0700

    Added test_random.py

---
+++ tests/modules/test_random.py
@@ -0,0 +1,8 @@
+import unittest
+
+class RandomTests(unittest.TestCase):
+
+    def test_choice(self):
+        numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+        random_number = random.choice(numbers)
+        self.assertIn(random_number, numbers)
