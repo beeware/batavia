6e58f9516b2f4cbacd03a1b4c9485bb4ececae37
Kartik Shah <krtk.6160@gmail.com>
2017-05-26 02:08:27 +0530

    Update test_len.py

--- tests/builtins/test_len.py
+++ tests/builtins/test_len.py
@@ -9,15 +9,5 @@ class BuiltinLenFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     functions = ["len"]
 
     not_implemented = [
-        'test_bool',
-        'test_bytes',
         'test_bytearray',
-        'test_class',
-        'test_complex',
-        'test_dict',
-        'test_frozenset',
-        'test_None',
-        'test_NotImplemented',
-        'test_set',
-        'test_slice',
     ]
