569996d40ed56404b58df9a7d36c13b9a3abdc25
Amir Rachum <amir@rachum.com>
2017-05-25 11:45:12 -0700

    Implement 'len' using '__len__'.

--- batavia/builtins/len.js
+++ batavia/builtins/len.js
@@ -1,6 +1,7 @@
 var exceptions = require('../core').exceptions
 var type_name = require('../core').type_name
 var types = require('../types')
+var callables = require('../core').callables
 
 function len(args, kwargs) {
     if (!args || args.length !== 1 || args[0] === undefined) {
@@ -9,18 +10,13 @@ function len(args, kwargs) {
 
     var value = args[0]
 
-    if (types.isinstance(value, [types.Float, types.Int])) {
-        throw new exceptions.TypeError.$pyclass("object of type '" + type_name(value) + "' has no len()")
+    if (value.__len__) {
+        return callables.call_method(value, '__len__', [])
     }
 
-    // if (args[0].hasOwnProperty("__len__")) {
-        // TODO: Fix context of python functions calling with proper vm
-        // throw new exceptions.NotImplementedError.$pyclass('Builtin Batavia len function is not supporting __len__ implemented.');
-        // return args[0].__len__.apply(vm);
-    // }
-
-    return new types.Int(args[0].length)
+    throw new exceptions.TypeError.$pyclass("object of type '" + type_name(value) + "' has no len()")
 }
+
 len.__doc__ = 'len(object)\n\nReturn the number of items of a sequence or collection.'
 
 module.exports = len
