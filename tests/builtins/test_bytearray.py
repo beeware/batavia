52e2f2832a03851733b27606a4ee0eed7d209297
ilankham <Isaiah.Lankham@gmail.com>
2017-05-25 13:40:55 -0700

    Cleanup comments

--- tests/builtins/test_bytearray.py
+++ tests/builtins/test_bytearray.py
@@ -8,21 +8,3 @@ class BytearrayTests(TranspileTestCase):
 class BuiltinBytearrayFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     functions = ["bytearray"]
     small_ints = True
-
-    not_implemented = [
-        # 'test_bytearray',
-        # 'test_class',
-        # 'test_complex',
-        # 'test_dict',
-        # 'test_float',
-        # 'test_frozenset',
-        # 'test_int',
-        # 'test_list',
-        # 'test_None',
-        # 'test_NotImplemented',
-        # 'test_range',
-        # 'test_set',
-        # 'test_slice',
-        # 'test_str',
-        # 'test_tuple',
-    ]
