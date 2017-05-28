3d0166128ebf797ef959957296875612e7f7e005
Jacob Stoebel <jstoebel@gmail.com>
2017-05-28 18:22:32 -0400

    start of str.format tests

--- tests/datatypes/test_str.py
+++ tests/datatypes/test_str.py
@@ -1052,6 +1052,58 @@ class FormatTests(TranspileTestCase):
 
             self.assertCodeExecution(tests, js_cleaner = js_cleaner, py_cleaner = py_cleaner)
 
+class NewStyleFormatTests(TranspileTestCase):
+
+    def test_single(self):
+        test_str = adjust("""
+        print(">>> 'Beeware is {}'.format('great!')")
+        'Beeware is {}'.format('great!')
+        """)
+
+        self.assertCodeExecution(test_str)  
+
+    def test_single_called_twice(self):
+        test_str = adjust("""
+        print(">>> 'Beeware is {} {} great!'.format('really')")
+        'Beeware is {} {} great!'.format('really')
+        """)
+
+        self.assertCodeExecution(test_str)
+
+    def test_by_index(self):
+        test_str = adjust("""
+        print(">>> 'Some numbers: {0} {1} {2}'.format('one', 'two', 'three')")
+        'Some numbers: {0} {1} {2}'.format('one', 'two', 'three')
+        print(">>> 'Some numbers: {0} {0} {0}'.format('one', 'two', 'three')")
+        'Some numbers: {0} {0} {0}'.format('one', 'two', 'three')
+        """)
+
+        self.assertCodeExecution(test_str)
+
+    def test_index_out_of_range(self):
+        test_str = adjust("""
+        print(">>> 'Some numbers: {0} {1} {5}'.format('one', 'two', 'three')")
+        'Some numbers: {0} {1} {5}'.format('one', 'two', 'three')
+        """)
+
+        self.assertCodeExecution(test_str)
+
+    def test_kwargs(self):
+        test_str = adjust("""
+        print(">>> '{food}! Lovely {food}! Lovely {food}!'.format(food='spam', other='eggs')")
+        '{food}! Lovely {food}! Lovely {food}!'.format(food='spam', other='eggs')
+        """)
+
+        self.assertCodeExecution(test_str)
+
+    def test_kwargs_key_erro(self):
+        test_str = adjust("""
+        print(">>> '{food}! Lovely {food}! Lovely {food}!'.format(other='eggs')")
+        '{food}! Lovely {food}! Lovely {food}!'.format(other='eggs')
+        """)
+
+        self.assertCodeExecution(test_str)
+        
 class UnaryStrOperationTests(UnaryOperationTestCase, TranspileTestCase):
     data_type = 'str'
 
