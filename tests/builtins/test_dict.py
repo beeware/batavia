c1ce53578864dfc70b9582ef78551ebf821f5b1d
Filipe Ximenes <filipeximenes@gmail.com>
2017-08-09 07:53:28 +1000

    fixes dict tests that passing due to the new native comparisson

--- tests/builtins/test_dict.py
+++ tests/builtins/test_dict.py
@@ -11,15 +11,9 @@ class BuiltinDictFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     not_implemented = [
         'test_bytearray',
         'test_bytes',
-        'test_class',
-        'test_complex',
-        'test_float',
         'test_frozenset',
-        'test_None',
-        'test_NotImplemented',
         'test_range',
         'test_set',
-        'test_slice',
         'test_str',
     ]
 
