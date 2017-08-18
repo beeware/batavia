b95d1dc0fbd170dab8f4e3b3ec81fc70f05c58f4
Eduardo Rivas <e@jerivas.com>
2017-08-18 13:38:41 -0700

    Implement built-in vars()

--- batavia/builtins/vars.js
+++ batavia/builtins/vars.js
@@ -1,7 +1,26 @@
 var exceptions = require('../core').exceptions
+var locals = require('./locals')
+var hasattr = require('./hasattr')
+var call_method = require('../core').callables.call_method
 
 function vars(args, kwargs) {
-    throw new exceptions.NotImplementedError.$pyclass("Builtin Batavia function 'vars' not implemented")
+    if (arguments.length !== 2) {
+        throw new exceptions.BataviaError.$pyclass('Batavia calling convention not used')
+    }
+    if (kwargs && Object.keys(kwargs).length > 0) {
+        throw new exceptions.TypeError.$pyclass("vars() doesn't accept keyword arguments")
+    }
+    if (args && args.length > 1) {
+        throw new exceptions.TypeError.$pyclass('vars() takes at most one argument (' + args.length + ' given)')
+    }
+    if (!args || args.length === 0) {
+        return locals()
+    }
+    var value = args[0]
+    if (!hasattr([value, '__dict__'])) {
+        throw new exceptions.TypeError.$pyclass('vars() argument must have __dict__ attribute')
+    }
+    return call_method(value, '__dict__')
 }
 vars.__doc__ = 'vars([object]) -> dictionary\n\nWithout arguments, equivalent to locals().\nWith an argument, equivalent to object.__dict__.'
 
--- tests/builtins/test_vars.py
+++ tests/builtins/test_vars.py
@@ -9,22 +9,5 @@ class BuiltinVarsFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     function = "vars"
 
     not_implemented = [
-        'test_noargs',
-        'test_bool',
-        'test_bytearray',
-        'test_bytes',
-        'test_class',
-        'test_complex',
-        'test_dict',
-        'test_float',
-        'test_frozenset',
-        'test_int',
-        'test_list',
-        'test_None',
-        'test_NotImplemented',
-        'test_range',
-        'test_set',
-        'test_slice',
-        'test_str',
-        'test_tuple',
+        "test_class",  # This will fail as long as <type> is missing __dict__
     ]
