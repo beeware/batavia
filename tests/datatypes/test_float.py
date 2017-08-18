fe791eb19d9c10671a430f4723bd61c3831e05df
Vic <vrootic@gmail.com>
2017-08-17 17:09:59 -0700

    Fixed test_add_int in test_float

--- tests/datatypes/test_float.py
+++ tests/datatypes/test_float.py
@@ -94,7 +94,7 @@ class InplaceFloatOperationTests(InplaceOperationTestCase, TranspileTestCase):
     not_implemented = [
 
         # these work, but print incorrectly
-        'test_add_int',
+        
         'test_floor_divide_int',
         'test_modulo_int',
         'test_subtract_int',
