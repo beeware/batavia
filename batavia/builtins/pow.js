bb311c27e6d54dfe06edcfb5c6a138e064b9fd63
Michael Noronha <michaeltnoronha@gmail.com>
2017-09-06 00:30:20 -0400

    Implement all remainin PowTests

--- batavia/builtins/pow.js
+++ batavia/builtins/pow.js
@@ -17,11 +17,11 @@ function pow(args, kwargs) {
 
         if (!types.isinstance(x, types.Int) ||
             !types.isinstance(y, types.Int) ||
-            !types.isinstance(y, types.Int)) {
-            throw new exceptions.TypeError.$pyclass('pow() requires all arguments be integers when 3 arguments are present')
+            !types.isinstance(z, types.Int)) {
+            throw new exceptions.TypeError.$pyclass('pow() 3rd argument not allowed unless all arguments are integers')
         }
         if (y < 0) {
-            throw new exceptions.TypeError.$pyclass('Builtin Batavia does not support negative exponents')
+            throw new exceptions.TypeError.$pyclass('pow() 2nd argument cannot be negative when 3rd argument specified')
         }
         if (y === 0) {
             return 1
