0c2da57f16a4b7480e9df03accf44eb879274460
Kshitij Sobti <kshitij@sobti.in>
2017-10-07 19:39:22 +0530

    Output null if module is not found Directly compile Python file given a path Add current directory to Python path so imports work

--- compile_module.py
+++ compile_module.py
@@ -1,18 +1,39 @@
 # coding=utf-8
-"""Compiles Python module and returns its file name and base64-encoded bytecode as JSON.
-For use by Batavia Loader."""
+"""Compile Python module and returns its file name and base64-encoded bytecode
+as JSON.
+For use by 'run_with_batavia' script."""
 
+import argparse
 import json
 import py_compile
-import argparse
+import sys
+
 import base64
 import importlib
 import os
 import tempfile
 
+sys.path.insert(0, os.getcwd())
 
-def python_module_to_b64_pyc(module_path):
-    module_file = importlib.util.find_spec(module_path).origin
+
+def get_module_path(module):
+    if module.endswith('.py'):
+        return module
+    else:
+        module_spec = importlib.util.find_spec(module)
+
+        # TODO: handle importing namespace packages
+        if module_spec is None or module_spec.origin == 'namespace':
+            return
+
+        return module_spec.origin
+
+
+def python_module_to_b64_pyc(module):
+    module_file = get_module_path(module)
+
+    if module_file is None:
+        return
 
     fp = tempfile.NamedTemporaryFile(delete=False)
     fp.close()
@@ -30,7 +51,7 @@ def python_module_to_b64_pyc(module_path):
     }
 
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('module', help='Python module')
 
@@ -38,3 +59,7 @@ if __name__ == '__main__':
 
     print(json.dumps(python_module_to_b64_pyc(args.module),
                      indent=4))
+
+
+if __name__ == '__main__':
+    main()
