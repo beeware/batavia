00ae087e2d8c52d4392c5fa275695b56dd3d3b79
Liel Fridman <lielft@gmail.com>
2017-10-29 02:18:42 +0200

    Increased bytearray() coverage to 50% of the tests. (#675)

--- tests/builtins/test_bytearray.py
+++ tests/builtins/test_bytearray.py
@@ -10,19 +10,9 @@ class BuiltinBytearrayFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
 
     not_implemented = [
         'test_noargs',
-        'test_bytearray',
-        'test_class',
-        'test_complex',
-        'test_dict',
-        'test_float',
-        'test_frozenset',
         'test_int',
         'test_list',
-        'test_None',
-        'test_NotImplemented',
         'test_range',
-        'test_set',
-        'test_slice',
         'test_str',
         'test_tuple',
     ]
