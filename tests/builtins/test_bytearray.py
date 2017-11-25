26f5d9358f4fa455e730f256964485a61dfdfbbb
Liel Fridman <lielft@gmail.com>
2017-11-25 19:40:35 +0200

    Completed the implementation of the bytearray builtin. (#681)

--- tests/builtins/test_bytearray.py
+++ tests/builtins/test_bytearray.py
@@ -7,12 +7,3 @@ class BytearrayTests(TranspileTestCase):
 
 class BuiltinBytearrayFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     function = "bytearray"
-
-    not_implemented = [
-        'test_noargs',
-        'test_int',
-        'test_list',
-        'test_range',
-        'test_str',
-        'test_tuple',
-    ]
