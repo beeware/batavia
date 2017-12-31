56761891a2b6cfd61895646d5f07f9734a6e18b4
Liel Fridman <lielft@gmail.com>
2017-12-31 21:06:39 +0200

    All the classmethod builtins now pass

--- batavia/builtins/classmethod.js
+++ batavia/builtins/classmethod.js
@@ -1,8 +1,30 @@
+var PyObject = require('../core/types/Object')
 var exceptions = require('../core').exceptions
+var type_name = require('../core').type_name
 
 function classmethod(args, kwargs) {
-    throw new exceptions.NotImplementedError.$pyclass(
-        "Builtin Batavia function 'classmethod' not implemented")
+    if (arguments.length !== 2) {
+        throw new exceptions.BataviaError.$pyclass('Batavia calling convention not used.')
+    }
+    if (kwargs && Object.keys(kwargs).length > 0) {
+        throw new exceptions.TypeError.$pyclass("classmethod() doesn't accept keyword arguments")
+    }
+    if (args.length !== 1) {
+        throw new exceptions.TypeError.$pyclass('classmethod expected 1 arguments, got ' + args.length)
+    }
+    var obj = new PyObject()
+    obj.toString = function() {
+        return '<classmethod object at 0xXXXXXXXX>'
+    }
+    obj.__str__ = obj.toString
+    obj.__repr__ = obj.toString
+    obj.__class__ = 'classmethod'
+    if (type_name(args[0]) === 'function') {
+        obj.__call__ = function() {
+            throw new exceptions.NotImplementedError.$pyclass('classmethod() can\'t get the parent class')
+        }
+    }
+    return obj
 }
 classmethod.__doc__ = 'classmethod(function) -> method\n\nConvert a function to be a class method.\n\nA class method receives the class as implicit first argument,\njust like an instance method receives the instance.\nTo declare a class method, use this idiom:\n\n  class C:\n      def f(cls, arg1, arg2, ...): ...\n      f = classmethod(f)\n\nIt can be called either on the class (e.g. C.f()) or on an instance\n(e.g. C().f()).  The instance is ignored except for its class.\nIf a class method is called for a derived class, the derived class\nobject is passed as the implied first argument.\n\nClass methods are different than C++ or Java static methods.\nIf you want those, see the staticmethod builtin.'
 
--- tests/builtins/test_classmethod.py
+++ tests/builtins/test_classmethod.py
@@ -7,24 +7,3 @@ class ClassmethodTests(TranspileTestCase):
 
 class BuiltinClassmethodFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     function = "classmethod"
-
-    not_implemented = [
-        'test_noargs',
-        'test_bool',
-        'test_bytearray',
-        'test_bytes',
-        'test_class',
-        'test_complex',
-        'test_dict',
-        'test_float',
-        'test_frozenset',
-        'test_int',
-        'test_list',
-        'test_None',
-        'test_NotImplemented',
-        'test_range',
-        'test_set',
-        'test_slice',
-        'test_str',
-        'test_tuple',
-    ]
