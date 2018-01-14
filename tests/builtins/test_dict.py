aa4355129573b291a6c01b4505e61be47d0b1516
Christopher Swenson <chris@caswenson.com>
2018-01-14 13:11:07 -0800

    Add known flakiness to set/frozenset -> dict conversion

--- tests/builtins/test_dict.py
+++ tests/builtins/test_dict.py
@@ -8,6 +8,15 @@ class DictTests(TranspileTestCase):
 class BuiltinDictFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     function = "dict"
 
+    # these are implemented, but the exact exception thrown depends on the order
+    # that they are iterated on being the exact same in both CPython and batavia,
+    # which is not guaranteed. (if a string with odd length is the "first" element, it gives
+    # a different exception than if an int is the "first" element)
+    is_flakey = [
+        'test_frozenset',
+        'test_set',
+    ]
+
     def test_well_formatted_set(self):
         self.assertCodeExecution("""
             good_set = {(1, 2), (2, 3)}
