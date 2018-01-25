4d6d9760071f9723492882fe9e355f7c81f1f22a
Liel Fridman <lielft@gmail.com>
2018-01-25 22:41:23 +0200

    The enumerate and a part of eval builtin tests now pass

--- batavia/builtins/eval.js
+++ batavia/builtins/eval.js
@@ -1,7 +1,21 @@
 var exceptions = require('../core').exceptions
+var types = require('../types')
 
 function eval_(args, kwargs) {
-    throw new exceptions.NotImplementedError.$pyclass("Builtin Batavia function 'eval' not implemented")
+    if (arguments.length !== 2) {
+        throw new exceptions.BataviaError.$pyclass('Batavia calling convention not used.')
+    }
+    if (kwargs && Object.keys(kwargs).length > 0) {
+        throw new exceptions.TypeError.$pyclass("eval() doesn't accept keyword arguments")
+    }
+    if (args.length === 0) {
+        throw new exceptions.TypeError.$pyclass('eval expected at least 1 arguments, got 0')
+    }
+    if (!types.isinstance(args[0], [types.Str, types.Bytes, types.Bytearray, types.Code])) {
+        throw new exceptions.TypeError.$pyclass('eval() arg 1 must be a string, bytes or code object')
+    }
+
+    throw new exceptions.NotImplementedError.$pyclass('eval() is not yet implemented in batavia.')
 }
 eval_.__doc__ = 'eval(source[, globals[, locals]]) -> value\n\nEvaluate the source in the context of globals and locals.\nThe source may be a string representing a Python expression\nor a code object as returned by compile().\nThe globals must be a dictionary and locals can be any mapping,\ndefaulting to the current globals and locals.\nIf only globals is given, locals defaults to it.\n'
 
--- tests/builtins/test_eval.py
+++ tests/builtins/test_eval.py
@@ -9,22 +9,7 @@ class BuiltinEvalFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     function = "eval"
 
     not_implemented = [
-        'test_noargs',
-        'test_bool',
         'test_bytearray',
         'test_bytes',
-        'test_class',
-        'test_complex',
-        'test_dict',
-        'test_float',
-        'test_frozenset',
-        'test_int',
-        'test_list',
-        'test_None',
-        'test_NotImplemented',
-        'test_range',
-        'test_set',
-        'test_slice',
         'test_str',
-        'test_tuple',
     ]
