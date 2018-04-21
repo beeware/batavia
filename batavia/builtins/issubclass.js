7441644971414362db0671169a22c7d9ce9183c6
marcus290 <36244342+marcus290@users.noreply.github.com>
2018-04-21 19:11:52 +0800

    issubclass implemented

--- batavia/builtins/issubclass.js
+++ batavia/builtins/issubclass.js
@@ -1,7 +1,19 @@
 var exceptions = require('../core').exceptions
+var types = require('../types')
 
-function issubclass() {
-    throw new exceptions.NotImplementedError.$pyclass("Builtin Batavia function 'issubclass' not implemented")
+function issubclass(args, kwargs) {
+    if (arguments.length !== 2) {
+        throw new exceptions.BataviaError.$pyclass("Batavia calling convention not used.")
+    }
+    if (kwargs && Object.keys(kwargs).length > 0) {
+        throw new exceptions.TypeError.$pyclass("issubclass() doesn't accept keyword arguments.")
+    }
+
+    if (!args || args.length !== 2) {
+        throw new exceptions.TypeError.$pyclass("issubclass() expected 2 arguments (" + args.length + " given)")
+    }
+
+    return new types.Bool(types.issubclass(args[0], args[1]))
 }
 issubclass.__doc__ = 'issubclass(C, B) -> bool\n\nReturn whether class C is a subclass (i.e., a derived class) of class B.\nWhen using a tuple as the second argument issubclass(X, (A, B, ...)),\nis a shortcut for issubclass(X, A) or issubclass(X, B) or ... (etc.).'
 
