634aa51fefa9f1626165704c852d98b12e00dddd
Sorensen, Nathan <nathan.sorensen@pioneer.com>
2018-05-14 14:18:00 -0500

    Implemented BinaryBytearrayOperationTests.test_add_int

--- batavia/types/Bytearray.js
+++ batavia/types/Bytearray.js
@@ -169,6 +169,7 @@ Bytearray.prototype.__mod__ = function(other) {
 
 Bytearray.prototype.__add__ = function(other) {
     var types = require('../types')
+
     if (types.isinstance(other, types.Int)) {
         throw new exceptions.TypeError.$pyclass("can't concat bytearray to " + type_name(other))
     }
