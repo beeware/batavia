d487e7797bbd4a16ec2bf3efcfde4574fd19f5d2
Nicholas Lambourne <ndl93@live.com>
2018-05-15 13:58:40 -0400

    all float() builtin error messages are python-compliant

--- batavia/builtins/float.js
+++ batavia/builtins/float.js
@@ -1,6 +1,8 @@
 var exceptions = require('../core').exceptions
+var str = require('./str')
 var type_name = require('../core').type_name
 var types = require('../types')
+
 
 function float(args, kwargs) {
     if (args.length > 1) {
@@ -12,8 +14,19 @@ function float(args, kwargs) {
 
     var value = args[0]
 
-    if (types.isinstance(value, types.Str)) {
-        if (value.length === 0) {
+    // For some reason complex throws a non-standard error message
+    if (types.isinstance(value, types.Complex)) {
+        throw new exceptions.TypeError.$pyclass("can't convert complex to float")
+    }
+
+    if (types.isinstance(value, [types.Bytes, types.Bytearray, types.Str])) {
+        // Attempt to convert bytearray to string for parsing.
+        var was_byte_object = false
+        if (types.isinstance(value, [types.Bytes, types.Bytearray])) {
+            was_byte_object = true
+            value = str([value], null)
+        }
+        if (value.length === 0 || value === "b''" || value === "bytearray''") {
             throw new exceptions.ValueError.$pyclass('could not convert string to float: ')
         } else if (value.search(/[^-0-9.]/g) === -1) {
             return new types.Float(parseFloat(value))
@@ -25,7 +38,12 @@ function float(args, kwargs) {
             } else if (value === '-inf') {
                 return new types.Float(-Infinity)
             }
-            throw new exceptions.ValueError.$pyclass("could not convert string to float: '" + args[0] + "'")
+            // Byte object errors are reported without quotations.
+            if (was_byte_object) {
+                throw new exceptions.ValueError.$pyclass('could not convert string to float: ' + value)
+            } else {
+                throw new exceptions.ValueError.$pyclass("could not convert string to float: '" + value + "'")
+            }
         }
     } else if (types.isinstance(value, [types.Int, types.Bool, types.Float])) {
         return args[0].__float__()
