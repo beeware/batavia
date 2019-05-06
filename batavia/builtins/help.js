278dd579671c44d916260dba32803e0afe69e274
Jessica Freasier <jfreasier@gmail.com>
2019-05-06 15:39:50 -0400

    fixed legacy references to pybee

--- batavia/builtins/help.js
+++ batavia/builtins/help.js
@@ -1,7 +1,7 @@
 
 function help(args, kwargs) {
     var sys = require('../modules/sys')
-    sys.stdout.write('For help, please see: https://github.com/pybee/batavia.')
+    sys.stdout.write('For help, please see: https://github.com/beeware/batavia.')
 }
 help.__doc__ = 'In Python, this is a wrapper around pydoc.help. In Batavia, this is a link to the README.'
 
