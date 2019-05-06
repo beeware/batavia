5ecab6f61ada87b79a43d640df96368f7e034168
Tyler Bindon <tbindon@google.com>
2019-05-06 11:50:46 -0400

    Remove more unexpected successes

--- tests/datatypes/test_bytes.py
+++ tests/datatypes/test_bytes.py
@@ -141,21 +141,4 @@ class InplaceBytesOperationTests(InplaceOperationTestCase, TranspileTestCase):
     data_type = 'bytes'
 
     not_implemented = [
-        'test_modulo_bool',
-        'test_modulo_bytearray',
-        'test_modulo_bytes',
-        'test_modulo_class',
-        'test_modulo_complex',
-        'test_modulo_dict',
-        'test_modulo_float',
-        'test_modulo_frozenset',
-        'test_modulo_int',
-        'test_modulo_list',
-        'test_modulo_None',
-        'test_modulo_NotImplemented',
-        'test_modulo_range',
-        'test_modulo_set',
-        'test_modulo_slice',
-        'test_modulo_str',
-        'test_modulo_tuple',
     ]
