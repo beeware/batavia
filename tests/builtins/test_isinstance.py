3546a00ef49a7c9c42d8990f9222e3ff07a4fb58
Tyler Bindon <tbindon@google.com>
2019-05-18 12:29:58 -0700

    Remove test_noargs from unimplemented now that the error is correct.

--- tests/builtins/test_isinstance.py
+++ tests/builtins/test_isinstance.py
@@ -35,91 +35,93 @@ class IsinstanceTests(TranspileTestCase):
 
 class BoolIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, bool)"
-    not_implemented = ["test_noargs"]
 
 
 class BytearrayIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, bytearray)"
-    not_implemented = ["test_noargs"]
 
 
 class BytesIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, bytes)"
-    not_implemented = ["test_noargs"]
 
 
 class ComplexIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, complex)"
-    not_implemented = ["test_noargs"]
 
 
 class DictIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, dict)"
-    not_implemented = ["test_noargs"]
 
 
 class FilterIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, filter)"
-    not_implemented = ["test_noargs"]
 
 
 class FloatIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, float)"
-    not_implemented = ["test_noargs"]
 
 
 class FrozensetIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, frozenset)"
-    not_implemented = ["test_noargs"]
 
 
 class IntIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, int)"
     not_implemented = [
         "test_bool",
-        "test_noargs",
     ]
 
 
 class ListIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, list)"
-    not_implemented = ["test_noargs"]
 
 
 class MapIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, map)"
-    not_implemented = ["test_noargs"]
 
 
-@unittest.expectedFailure
 class MemoryviewIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, memoryview)"
-    not_implemented = ["test_noargs"]
+
+    not_implemented = [
+        "test_None",
+        "test_NotImplemented",
+        "test_bool",
+        "test_bytearray",
+        "test_bytes",
+        "test_class",
+        "test_complex",
+        "test_dict",
+        "test_float",
+        "test_frozenset",
+        "test_int",
+        "test_list",
+        "test_range",
+        "test_set",
+        "test_slice",
+        "test_str",
+        "test_tuple",
+    ]
 
 
 class RangeIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, range)"
-    not_implemented = ["test_noargs"]
 
 
 class SetIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, set)"
-    not_implemented = ["test_noargs"]
 
 
 class StrIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, str)"
-    not_implemented = ["test_noargs"]
 
 
 class TupleIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, tuple)"
-    not_implemented = ["test_noargs"]
 
 
 class TypeIsinstanceFunctionTest(BuiltinFunctionTestCase, TranspileTestCase):
     function = "lambda a: isinstance(a, type)"
-    not_implemented = ["test_noargs"]
 
 
 class BuiltinIsinstanceFunctionTests(BuiltinTwoargFunctionTestCase, TranspileTestCase):
