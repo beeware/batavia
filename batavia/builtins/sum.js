619109c760ccf0361a3e403486053d3550b820f1
Tyler Bindon <tbindon@google.com>
2019-05-27 13:22:29 -0700

    Get all existing tests passing for sum and add two arg tests.

--- batavia/builtins/sum.js
+++ batavia/builtins/sum.js
@@ -1,20 +1,17 @@
-var operators = require('../operators')
+var callables = require('../core').callables
 var exceptions = require('../core').exceptions
+var operators = require('../operators')
 var types = require('../types')
 var type_name = require('../core').type_name
 
 const ADD_OPERATOR = operators['__add__']
-
-function add(left, right) {
-    return ADD_OPERATOR.apply(left, right)
-}
 
 function sum(args, kwargs) {
     if (arguments.length !== 2) {
         throw new exceptions.BataviaError.$pyclass('Batavia calling convention not used.')
     }
     if (kwargs && Object.keys(kwargs).length > 0) {
-        throw new exceptions.TypeError.$pyclass("sum() doesn't accept keyword arguments")
+        throw new exceptions.TypeError.$pyclass('sum() takes no keyword arguments')
     }
     if (!args || args.length === 0) {
         throw new exceptions.TypeError.$pyclass('sum expected at least 1 arguments, got ' + args.length)
@@ -22,55 +19,31 @@ function sum(args, kwargs) {
     if (args.length > 2) {
         throw new exceptions.TypeError.$pyclass('sum expected at most 2 arguments, got ' + args.length)
     }
-    if (!args[0].__iter__) {
-        throw new exceptions.TypeError.$pyclass("'" + type_name(args[0]) + "' object is not iterable")
+
+    const builtins = require('../builtins')
+    const iterator = builtins.iter([args[0]], {})
+
+    let sum
+    if (args.length === 2) {
+        if (types.isinstance(args[1], [types.Str])) {
+            throw new exceptions.TypeError.$pyclass('sum() can\'t sum strings [use \'\'.join(seq) instead]')
+        }
+        if (types.isinstance(args[1], [types.Bytes, types.Bytearray])) {
+            throw new exceptions.TypeError.$pyclass(
+                'sum() can\'t sum ' + type_name(args[1]) + ' [use b\'\'.join(seq) instead]')
+        }
+        sum = args[1]
+    } else {
+        sum = new types.Int(0)
     }
-    let value = args[0]
-    // This is unique behaviour that is handled before types are resolved and added.
-    // Empty strings, sets resolve to zero when provided as a single argument to sum().
-    let resolve_to_zero_types = [types.Set, types.Str]
-    if (types.isinstance(value, resolve_to_zero_types)) {
-        if (value === '' || (types.isinstance(value, types.Set) && value.toString() === (new types.Set()).toString())) {
-            return new types.Int(0)
-        } else if (types.isinstance(value, types.Str)) {
-            // Reject all non-empty strings.
-            return add(new types.Int(0), value) // This will throw the correct TypeError.
-        }
-    }
-    // Sets need to be handled differently to other iterable types due to use of dict for data.
-    if (types.isinstance(value, [types.Set, types.FrozenSet])) {
-        return value.data.keys().reduce(function(a, b) {
-            return add(a, b)
-        }, new types.Int(0))
-    } else if (types.isinstance(value, types.Range)) {
-        // This doesn't work yet due to upstream error.
-        let retval = types.Int(0)
-        for (let i = value.start; i < value.end; i += value.step) {
-            retval = add(retval, new types.Int(value.val[i]))
-        }
-        return retval
-    } else if (types.isinstance(value, types.Dict)) {
-        // Sum of dict is sum of keys.
-        return value.keys().reduce(function(a, b) {
-            return add(a, b)
-        }, new types.Int(0))
-    } else if (types.isinstance(value, [types.Bytes, types.Bytearray])) {
-        // Sum of bytearray is sum of internal value.
-        if (types.isinstance(value, types.Bytearray)) {
-            value = value.valueOf()
-        }
-        let retval = new types.Int(0)
-        // Sum of non-empty byte-strings is the sum of ASCII values of each byte.
-        if (value.__len__() > 0) {
-            for (let i = 0; i < value.__len__(); i++) {
-                retval = add(retval, new types.Int(value.val[i]))
-            }
-        }
-        return retval
-    }
-    return value.reduce(add, new types.Int(0))
+
+    callables.iter_for_each(iterator, (value) => {
+        sum = ADD_OPERATOR.apply(sum, value)
+    })
+
+    return sum
 }
 
-sum.__doc__ = "sum(iterable[, start]) -> value\n\nReturn the sum of an iterable of numbers (NOT strings) plus the value\nof parameter 'start' (which defaults to 0).  When the iterable is\nempty, return start."
+sum.__doc__ = 'sum(iterable[, start]) -> value\n\nReturn the sum of an iterable of numbers (NOT strings) plus the value\nof parameter \'start\' (which defaults to 0).  When the iterable is\nempty, return start.'
 
 module.exports = sum
--- tests/builtins/test_sum.py
+++ tests/builtins/test_sum.py
@@ -1,6 +1,4 @@
-from unittest import expectedFailure
-
-from .. utils import TranspileTestCase, BuiltinFunctionTestCase
+from ..utils import TranspileTestCase, BuiltinFunctionTestCase, BuiltinTwoargFunctionTestCase
 
 
 class SumTests(TranspileTestCase):
@@ -19,7 +17,6 @@ class SumTests(TranspileTestCase):
             print(sum((1, 2, 3, 4, 5, 6, 7)))
         """)
 
-    @expectedFailure
     def test_sum_iterator(self):
         self.assertCodeExecution("""
             i = iter([1, 2])
@@ -30,6 +27,14 @@ class SumTests(TranspileTestCase):
     def test_sum_mix_floats_and_ints(self):
         self.assertCodeExecution("""
             print(sum([1, 1.414, 2, 3.14159]))
+        """)
+
+    def test_sum_kwargs(self):
+        self.assertCodeExecution("""
+            try:
+                print(sum(x=1))
+            except TypeError as e:
+                print(e)
         """)
 
 
@@ -46,6 +51,38 @@ class BuiltinSumFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
         'test_set',  # This works, but python dict.keys() returns non-deterministically.
     ]
 
-    not_implemented = [
-        'test_range',  # This has been implemented, but fails upstream on isinstance.
+
+class BuiltinSumTwoArgFunctionTests(BuiltinTwoargFunctionTestCase, TranspileTestCase):
+    function = "sum"
+
+    # these are implemented, but the exact exception thrown depends on the order
+    # that they are iterated on being the exact same in both CPython and batavia,
+    # which is not guaranteed. (if an unsupported string follows an int, the error
+    # will be different than if it followed a float)
+
+    is_flakey = [
+        "test_frozenset_None",
+        "test_frozenset_NotImplemented",
+        "test_frozenset_bool",
+        "test_frozenset_class",
+        "test_frozenset_dict",
+        "test_frozenset_frozenset",
+        "test_frozenset_int",
+        "test_frozenset_list",
+        "test_frozenset_range",
+        "test_frozenset_set",
+        "test_frozenset_slice",
+        "test_frozenset_tuple",
+        "test_set_None",
+        "test_set_NotImplemented",
+        "test_set_bool",
+        "test_set_class",
+        "test_set_dict",
+        "test_set_frozenset",
+        "test_set_int",
+        "test_set_list",
+        "test_set_range",
+        "test_set_set",
+        "test_set_slice",
+        "test_set_tuple",
     ]
