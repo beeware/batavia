91c9633bd12fa40fc75dd50374879d2cbc113be1
Tyler Bindon <tbindon@google.com>
2019-06-02 12:02:30 -0700

    Remove 3.4 stuff and add 3.5 and 3.6 doc strings.

--- batavia/builtins/format.js
+++ batavia/builtins/format.js
@@ -6,15 +6,9 @@ function format(args, kwargs) {
         throw new exceptions.BataviaError.$pyclass('Batavia calling convention not used.')
     }
     if (!args || args.length === 0) {
-        if (version.later('3.4')) {
-            throw new exceptions.TypeError.$pyclass(
-                'format() takes exactly at least one argument (' + args.length + ' given)'
-            )
-        } else {
-            throw new exceptions.TypeError.$pyclass(
-                'format() takes at least 1 argument (' + args.length + ' given)'
-            )
-        }
+        throw new exceptions.TypeError.$pyclass(
+            'format() takes at least 1 argument (' + args.length + ' given)'
+        )
     }
     if (args.length > 2) {
         throw new exceptions.TypeError.$pyclass(
@@ -29,6 +23,10 @@ function format(args, kwargs) {
     return args[0].__format__(args[0], args[1]) // TODO: Implement the __format__ function for types like int and string, where it actually can do something
 }
 
-format.__doc__ = 'format(value[, format_spec]) -> string\n\nReturns value.__format__(format_spec)\nformat_spec defaults to ""'
+if (!version.earlier('3.6') {
+    format.__doc__ = "Return value.__format__(format_spec)\n\nformat_spec defaults to the empty string.\nSee the Format Specification Mini-Language section of help('FORMATTING') for\ndetails."
+} else {
+    format.__doc__ = 'Return value.__format__(format_spec)\n\nformat_spec defaults to the empty string'
+}			
 
 module.exports = format
