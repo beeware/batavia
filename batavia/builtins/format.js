6322d9147e6eea1a10165afe5b32a4475d71d350
Tyler Bindon <tbindon@google.com>
2019-06-04 19:26:04 -0700

    version.earlier doesn't work at require time. The version isn't set. :(

--- batavia/builtins/format.js
+++ batavia/builtins/format.js
@@ -1,4 +1,3 @@
-var version = require('../core').version
 var exceptions = require('../core').exceptions
 
 function format(args, kwargs) {
@@ -23,10 +22,6 @@ function format(args, kwargs) {
     return args[0].__format__(args[0], args[1]) // TODO: Implement the __format__ function for types like int and string, where it actually can do something
 }
 
-if (!version.earlier('3.6')) {
-    format.__doc__ = "Return value.__format__(format_spec)\n\nformat_spec defaults to the empty string.\nSee the Format Specification Mini-Language section of help('FORMATTING') for\ndetails."
-} else {
-    format.__doc__ = 'Return value.__format__(format_spec)\n\nformat_spec defaults to the empty string'
-}
+format.__doc__ = 'Return value.__format__(format_spec)\n\nformat_spec defaults to the empty string'
 
 module.exports = format
