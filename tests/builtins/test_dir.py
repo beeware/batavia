4dc46aae541cd5957c2cb3e3ffc39b22600b11f4
Tyler Bindon <tbindon@google.com>
2019-06-08 08:37:07 -0700

    Update all __dir__ properties to return lists instead of strings.

--- tests/builtins/test_dir.py
+++ tests/builtins/test_dir.py
@@ -8,10 +8,6 @@ class DirTests(TranspileTestCase):
 class BuiltinDirFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     function = "dir"
 
-    not_implemented_versions = {
-        'test_noargs': ['3.6'],
-    }
-
     not_implemented = [
         'test_class',
     ]
