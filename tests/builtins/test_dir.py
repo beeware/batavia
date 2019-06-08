1f482d75e0cbc36d28f3e1efdcf692da0168f4ca
Tyler Bindon <tbindon@google.com>
2019-06-08 08:53:19 -0700

    Add test for dir() return type.

--- tests/builtins/test_dir.py
+++ tests/builtins/test_dir.py
@@ -11,3 +11,12 @@ class BuiltinDirFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     not_implemented = [
         'test_class',
     ]
+
+
+class BuiltinDirTypeFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
+    """Test suite that ensures that dir(x) returns the correct type."""
+    function = "lambda x: type(dir(x))"
+
+    not_implemented = [
+        'test_class',
+    ]
