e00cfca7a59f04d24eb49b35d4e736a0219fa75c
Joel <staujd02@students.ipfw.edu>
2019-06-12 18:14:29 -0400

    Fixed formatting bugs
    Signed-off-by: Joel <staujd02@students.ipfw.edu>

--- tests/builtins/test_format.py
+++ tests/builtins/test_format.py
@@ -1,5 +1,6 @@
 from .. utils import TranspileTestCase, BuiltinFunctionTestCase
 
+
 class FormatTests(TranspileTestCase):
     
     def test_format_throws_error_if_given_zero_args(self):
@@ -8,7 +9,7 @@ class FormatTests(TranspileTestCase):
                 print(format())
             except TypeError as error:
                 print(error)
-        """, run_in_function=False) 
+        """, run_in_function=False)
 
     def test_format_throws_an_error_if_given_too_many_arguments(self):
         self.assertCodeExecution("""
@@ -20,13 +21,14 @@ class FormatTests(TranspileTestCase):
                 print(format("Way","Too","Many","Args"))
             except TypeError as error:
                 print(error)
-        """, run_in_function=False) 
+        """, run_in_function=False)
 
     def test_format_with_empty_string_for_second_arg_is_same_as_str(self):
         self.assertCodeExecution("""
             print(format("string", ""))
             # print(format("string", "") == type("string").__format__("string",""))
-        """, run_in_function=False) 
+        """, run_in_function=False)
+
 
 class BuiltinFormatFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     function = "format"
