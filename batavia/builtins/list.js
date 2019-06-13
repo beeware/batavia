c2971c036c283b41353e7e8e3554340306cf7f4c
awildtechno <ajdweavers@gmail.com>
2019-06-12 22:29:54 -0700

    Handle kwargs

--- batavia/builtins/list.js
+++ batavia/builtins/list.js
@@ -1,11 +1,21 @@
 var types = require('../types')
+var exceptions = require('../core/exceptions')
 
 var list = types.List.prototype.__class__
 
-list.__call__ = function(args) {
+list.__call__ = function(args, kwargs) {
+    // Reject kwargs
+    if (kwargs && Object.keys(kwargs).length) {
+        if (Object.keys(kwargs).length + args.length > 1) {
+            throw new exceptions.TypeError.$pyclass('list() takes at most 1 argument (' + (args.length + Object.keys(kwargs).length) + ' given)')
+        } else {
+            throw new exceptions.TypeError.$pyclass("'" + (args.length + Object.keys(kwargs)[0]) + "' is an invalid keyword argument for this function")
+        }
+    }
     if (!args || args.length === 0) {
         return new types.List()
     }
+    
     return new types.List(args[0])
 }
 list.__doc__ = "list() -> new empty list\nlist(iterable) -> new list initialized from iterable's items"
