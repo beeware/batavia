a780e743ac2091439bd0dadc8dc5db5444d59fa8
awildtechno <ajdweavers@gmail.com>
2019-06-12 22:42:48 -0700

    Enforce calling convention

--- batavia/builtins/list.js
+++ batavia/builtins/list.js
@@ -4,12 +4,15 @@ var exceptions = require('../core/exceptions')
 var list = types.List.prototype.__class__
 
 list.__call__ = function(args, kwargs) {
-    // Reject kwargs
+    if (arguments.length !== 2) {
+        throw new builtins.BataviaError.$pyclass("Batavia calling convention not used.");
+    }
+
     if (kwargs && Object.keys(kwargs).length) {
         if (Object.keys(kwargs).length + args.length > 1) {
             throw new exceptions.TypeError.$pyclass('list() takes at most 1 argument (' + (args.length + Object.keys(kwargs).length) + ' given)')
         } else {
-            throw new exceptions.TypeError.$pyclass("'" + (args.length + Object.keys(kwargs)[0]) + "' is an invalid keyword argument for this function")
+            throw new exceptions.TypeError.$pyclass("'" + Object.keys(kwargs)[0] + "' is an invalid keyword argument for this function")
         }
     }
     if (!args || args.length === 0) {
