d4957c35d3058c37302ce572e66ced6601e2d7f5
Greg Cowell <greg.cowell@gmail.com>
2020-04-25 16:11:51 +1000

    Implement staticmethod (#831)
    Signed-off-by: Greg Cowell <greg.cowell@gmail.com>

--- batavia/builtins/staticmethod.js
+++ batavia/builtins/staticmethod.js
@@ -1,8 +1,46 @@
+var PyObject = require('../core/types/Object')
 var exceptions = require('../core').exceptions
+var type_name = require('../core').type_name
 
 function staticmethod(args, kwargs) {
-    throw new exceptions.NotImplementedError.$pyclass("Builtin Batavia function 'staticmethod' not implemented")
+    if (arguments.length !== 2) {
+        throw new exceptions.BataviaError.$pyclass('Batavia calling convention not used.')
+    }
+    if (kwargs && Object.keys(kwargs).length > 0) {
+        throw new exceptions.TypeError.$pyclass("staticmethod() doesn't accept keyword arguments")
+    }
+    if (args.length !== 1) {
+        throw new exceptions.TypeError.$pyclass('staticmethod expected 1 arguments, got ' + args.length)
+    }
+    var obj = new PyObject();
+    obj.toString = function() {
+        return '<staticmethod object at 0xXXXXXXXX>'
+    }
+    obj.__str__ = obj.toString;
+    obj.__repr__ = obj.toString;
+    obj.__class__ = 'staticmethod';
+    if (type_name(args[0]) === 'function') {
+        obj.__call__ = args[0].__call__;
+    }
+    return obj;
 }
-staticmethod.__doc__ = 'staticmethod(function) -> method\n\nConvert a function to be a static method.\n\nA static method does not receive an implicit first argument.\nTo declare a static method, use this idiom:\n\n     class C:\n     def f(arg1, arg2, ...): ...\n     f = staticmethod(f)\n\nIt can be called either on the class (e.g. C.f()) or on an instance\n(e.g. C().f()).  The instance is ignored except for its class.\n\nStatic methods in Python are similar to those found in Java or C++.\nFor a more advanced concept, see the classmethod builtin.'
+
+staticmethod.__doc__ = 'staticmethod(function) -> method\n' +
+'\n' +
+'Convert a function to be a static method.\n' +
+'\n' +
+'A static method does not receive an implicit first argument.\n' +
+'To declare a static method, use this idiom:\n' +
+'\n' +
+'     class C:\n' +
+'         @staticmethod\n' +
+'         def f(arg1, arg2, ...):\n' +
+'             ...\n' +
+'\n' +
+'It can be called either on the class (e.g. C.f()) or on an instance\n' +
+'(e.g. C().f()).  The instance is ignored except for its class.\n' +
+'\n' +
+'Static methods in Python are similar to those found in Java or C++.\n' +
+'For a more advanced concept, see the classmethod builtin.\n';
 
 module.exports = staticmethod
--- tests/builtins/test_staticmethod.py
+++ tests/builtins/test_staticmethod.py
@@ -7,24 +7,3 @@ class StaticmethodTests(TranspileTestCase):
 
 class BuiltinStaticmethodFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     function = "staticmethod"
-
-    not_implemented = [
-        'test_noargs',
-        'test_bool',
-        'test_bytearray',
-        'test_bytes',
-        'test_class',
-        'test_complex',
-        'test_dict',
-        'test_float',
-        'test_frozenset',
-        'test_int',
-        'test_list',
-        'test_None',
-        'test_NotImplemented',
-        'test_range',
-        'test_set',
-        'test_slice',
-        'test_str',
-        'test_tuple',
-    ]
